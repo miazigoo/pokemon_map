72ccbf9b65ba3edd1037c8761f5fd319e578ad2c
Bloodov Pavel <kasper6024@gmail.com>
2023-04-05 02:12:56 +0300

    Исправлены поля моделей

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,10 +4,10 @@ from django.utils.safestring import mark_safe
 
 class Pokemon(models.Model):
     title = models.CharField(verbose_name="Покемон_ru", max_length=200)
-    title_en = models.CharField(verbose_name="Покемон_en", max_length=200, null=True)
-    title_jp = models.CharField(verbose_name="Покемон_jp", max_length=200, null=True)
-    photo = models.ImageField(verbose_name="Картинка", upload_to='pokemons', null=True)
-    description = models.TextField(verbose_name='Описание', null=True)
+    title_en = models.CharField(verbose_name="Покемон_en", max_length=200, blank=True)
+    title_jp = models.CharField(verbose_name="Покемон_jp", max_length=200, blank=True)
+    photo = models.ImageField(verbose_name="Картинка", upload_to='pokemons', blank=True)
+    description = models.TextField(verbose_name='Описание')
     previous_evolution = models.ForeignKey("Pokemon", on_delete=models.CASCADE, blank=True, null=True,
                                            verbose_name="Из кого эволюционирует", related_name='next_evolutions')
     next_evolution = models.ForeignKey("Pokemon", on_delete=models.CASCADE, blank=True, null=True,
@@ -27,10 +27,10 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name="Покемон")
     lat = models.FloatField(verbose_name="Широта")
     lon = models.FloatField(verbose_name="Долгота")
-    appeared_at = models.DateTimeField(verbose_name="Появится в:")
-    disappeared_at = models.DateTimeField(verbose_name="Исчезнет в:")
-    level = models.IntegerField(verbose_name="Уровень:", null=True)
-    health = models.IntegerField(verbose_name="Здоровье:", null=True)
-    strength = models.IntegerField(verbose_name="Атака:", null=True)
-    defense = models.IntegerField(verbose_name="Защита:", null=True)
-    stamina = models.IntegerField(verbose_name="Выносливость:", null=True)
+    appeared_at = models.DateTimeField(verbose_name="Появится в:", blank=True)
+    disappeared_at = models.DateTimeField(verbose_name="Исчезнет в:", blank=True)
+    level = models.IntegerField(verbose_name="Уровень:")
+    health = models.IntegerField(verbose_name="Здоровье:")
+    strength = models.IntegerField(verbose_name="Атака:")
+    defense = models.IntegerField(verbose_name="Защита:")
+    stamina = models.IntegerField(verbose_name="Выносливость:")
